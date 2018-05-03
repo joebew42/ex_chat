8a9c4d224950b81a240445105cfed3da3c39e3b4
JoeBew42 <joebew42@gmail.com>
2018-05-03 20:02:53 +0200

    change the representation of the command

--- lib/ex_chat/web/chatroom_websocket_handler.ex
+++ lib/ex_chat/web/chatroom_websocket_handler.ex
@@ -11,20 +11,8 @@ defmodule ExChat.Web.ChatRoomWebSocketHandler do
     {:ok, req, state}
   end
 
-  def websocket_handle({:text, "join"}, req, state) do
-    :ok = ExChat.ChatRooms.join("default", self())
-
-    response = %{
-      room: "default",
-      message: "welcome to the default chat room!"
-    }
-
-    {:reply, {:text, as_json(response)}, req, state}
-  end
-
-  def websocket_handle({:text, message}, req, state) do
-    :ok = ExChat.ChatRooms.send("default", message)
-    {:ok, req, state}
+  def websocket_handle({:text, command_as_json}, req, state) do
+    handle(from_json(command_as_json), req, state)
   end
 
   def websocket_handle(_message, req, state) do
@@ -32,12 +20,34 @@ defmodule ExChat.Web.ChatRoomWebSocketHandler do
   end
 
   def websocket_info(message, req, state) do
-    {:reply, {:text, message}, req, state}
+    response = %{
+      room: "default",
+      message: message
+    }
+
+    {:reply, {:text, to_json(response)}, req, state}
   end
 
   def websocket_terminate(_reason, _req, _state) do
     :ok
   end
 
-  defp as_json(response), do: Poison.encode!(response)
+  defp handle(%{"command" => "join"}, req, state) do
+    :ok = ExChat.ChatRooms.join("default", self())
+
+    response = %{
+      room: "default",
+      message: "welcome to the default chat room!"
+    }
+
+    {:reply, {:text, to_json(response)}, req, state}
+  end
+
+  defp handle(%{"room" => room, "message" => message}, req, state) do
+    :ok = ExChat.ChatRooms.send(room, message)
+    {:ok, req, state}
+  end
+
+  defp to_json(response), do: Poison.encode!(response)
+  defp from_json(json), do: Poison.decode!(json)
 end
--- test/ex_chat/web/router_test.exs
+++ test/ex_chat/web/router_test.exs
@@ -12,7 +12,7 @@ defmodule ExChat.Web.RouterTest do
   describe "when join the default chat room" do
     setup do
       {:ok, ws_client} = connect_to "ws://localhost:4000/room", forward_to: self()
-      send_as_text(ws_client, "join")
+      send_as_text(ws_client, "{\"command\":\"join\"}")
 
       {:ok, ws_client: ws_client}
     end
@@ -29,12 +29,11 @@ defmodule ExChat.Web.RouterTest do
 
     test "we receive all the messages sent by other clients" do
       {:ok, other_client} = connect_to "ws://localhost:4000/room", forward_to: NullProcess.start
-      send_as_text(other_client, "join")
+      send_as_text(other_client, "{\"command\":\"join\"}")
 
       send_as_text(other_client, "{\"room\":\"default\",\"message\":\"Hello from Twitch!\"}")
 
       assert_receive "{\"room\":\"default\",\"message\":\"Hello from Twitch!\"}"
     end
   end
-
 end
