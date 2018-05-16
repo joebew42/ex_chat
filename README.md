872cd016d0935a74d7f5ec0d4bd868a079df13e1
JoeBew42 <joebew42@gmail.com>
2018-05-16 20:49:18 +0200

    rename subscribers to clients

--- README.md
+++ README.md
@@ -29,9 +29,9 @@ _Check out the chat web client at `http://localhost:4000/chat.html`_
 
 ### DOING
 
+
 ### TODO
 
-- rename `subscribers` to `clients` in `ChatRoom`
 - unsubscribe a client to receive messages once it leaves the chat
 - handle invalid client messages
 - improve the way we make assertions on received messages (e.g. assert_receive wants pattern match and not functions or variables) in the `websocket_test.exs`
@@ -45,6 +45,7 @@ _Check out the chat web client at `http://localhost:4000/chat.html`_
 
 ### DONE
 
+- rename `subscribers` to `clients` in `ChatRoom`
 - bug: avoid that a client can join twice to a room
 - add a `Supervisor` to supervise all the `ChatRoom` processes
 - use a [registry](https://hexdocs.pm/elixir/master/Registry.html) to name all the `ChatRoom` processes
--- lib/ex_chat/chat_room.ex
+++ lib/ex_chat/chat_room.ex
@@ -1,7 +1,7 @@
 defmodule ExChat.ChatRoom do
   use GenServer
 
-  defstruct subscribers: [], name: nil
+  defstruct clients: [], name: nil
 
   def create(name) do
     GenServer.start_link(__MODULE__, %__MODULE__{name: name}, name: via_registry(name))
@@ -17,38 +17,32 @@ defmodule ExChat.ChatRoom do
     Registry.lookup(ExChat.Registry, room)
   end
 
-  def join(pid, subscriber) do
-    GenServer.call(pid, {:join, subscriber})
+  def join(pid, client) do
+    GenServer.call(pid, {:join, client})
   end
 
   def send(pid, message) do
     :ok = GenServer.cast(pid, {:send, message})
   end
 
-  def handle_call({:join, subscriber}, _from, state) do
-    {message, new_state} = case Enum.member?(state.subscribers, subscriber) do
-      true ->
-        {{:error, :already_joined}, state}
-      false ->
-        {:ok, add_subscriber(state, subscriber)}
+  def handle_call({:join, client}, _from, state) do
+    {message, new_state} = case joined?(state.clients, client) do
+      true -> {{:error, :already_joined}, state}
+      false -> {:ok, add_client(state, client)}
     end
 
     {:reply, message, new_state}
   end
 
-  def handle_call({:joined?, client}, _from, state) do
-    result = Enum.member?(state.subscribers, client)
-
-    {:reply, result, state}
-  end
-
   def handle_cast({:send, message}, state = %__MODULE__{name: name}) do
-    Enum.each(state.subscribers, &Kernel.send(&1, {name, message}));
+    Enum.each(state.clients, &Kernel.send(&1, {name, message}));
     {:noreply, state}
   end
 
-  defp add_subscriber(state = %__MODULE__{subscribers: subscribers}, subscriber) do
-    %__MODULE__{state | subscribers: [subscriber|subscribers]}
+  defp joined?(clients, client), do: Enum.member?(clients, client)
+
+  defp add_client(state = %__MODULE__{clients: clients}, client) do
+    %__MODULE__{state | clients: [client|clients]}
   end
 
   defp via_registry(name), do: {:via, Registry, {ExChat.Registry, name}}
