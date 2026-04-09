3254b6516364b7fd194db36aeca984b7f6afec86
Joe Bew <joebew42@gmail.com>
2026-04-09 14:13:16 +0200

    chore: remove hardcoded seed users from setup

--- lib/ex_chat/setup.ex
+++ lib/ex_chat/setup.ex
@@ -1,7 +1,7 @@
 defmodule ExChat.Setup do
   use Task, restart: :transient
 
-  alias ExChat.{ChatRooms, UserSessions, AccessTokenRepository}
+  alias ExChat.ChatRooms
 
   def start_link(_args) do
     Task.start_link(__MODULE__, :run, [])
@@ -9,11 +9,5 @@ defmodule ExChat.Setup do
 
   def run() do
     ChatRooms.create("default")
-
-    UserSessions.create("foo_user")
-    UserSessions.create("bar_user")
-
-    AccessTokenRepository.add("foo_token", "foo_user")
-    AccessTokenRepository.add("bar_token", "bar_user")
   end
 end
