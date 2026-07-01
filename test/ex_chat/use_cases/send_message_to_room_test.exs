a7b4a8334d51da72268a5d65c982274391b0294f
Joe Bew <joebew42@gmail.com>
2026-07-01 08:09:19 +0200

    refactor: rename SendMessageToChatRoom use case to SendMessageToRoom

--- test/ex_chat/use_cases/send_message_to_room_test.exs
+++ test/ex_chat/use_cases/send_message_to_room_test.exs
@@ -1,14 +1,14 @@
-defmodule ExChat.UseCases.SendMessageToChatRoomTest do
+defmodule ExChat.UseCases.SendMessageToRoomTest do
   use ExUnit.Case, async: true
 
   import Mock
 
   alias ExChat.Rooms
-  alias ExChat.UseCases.SendMessageToChatRoom
+  alias ExChat.UseCases.SendMessageToRoom
 
   test "return an error message when the room does not exists" do
     with_mock(Rooms, send: fn(_, _) -> {:error, :unexisting_room} end) do
-      result = SendMessageToChatRoom.on("a message", "a room", "a user id")
+      result = SendMessageToRoom.on("a message", "a room", "a user id")
 
       assert result == {:error, "a room does not exists"}
       assert called Rooms.send("a message", to: "a room", as: "a user id")
@@ -17,7 +17,7 @@ defmodule ExChat.UseCases.SendMessageToChatRoomTest do
 
   test "return ok when send a message" do
     with_mock(Rooms, send: fn(_, _) -> :ok end) do
-      result = SendMessageToChatRoom.on("a message", "a room", "a user id")
+      result = SendMessageToRoom.on("a message", "a room", "a user id")
 
       assert result == :ok
       assert called Rooms.send("a message", to: "a room", as: "a user id")
